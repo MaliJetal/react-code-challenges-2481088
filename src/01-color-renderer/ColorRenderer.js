5b0e6eb74efb3ba4f0372290b350a80b120c791c
Jetal Mali <jetmali@publicisgroupe.net>
2022-10-23 21:23:02 +0530

    Challenge 1 - My Solution

--- src/01-color-renderer/ColorRenderer.js
+++ src/01-color-renderer/ColorRenderer.js
@@ -1,27 +1,34 @@
-import Color from './Color'
+import Color from "./Color";
 
-const colors = [{
-  hex: '#91A6FF',
-  name: 'Cornflower Blue'
-},
-{
-  hex: '#FF88DC',
-  name: 'Persian Pink'
-},
-{
-  hex: '#80FF72',
-  name: 'Screamin Green'
-},
-{
-  hex: '#FF5154',
-  name: 'Tart Orange'
-}]
+const colors = [
+	{
+		hex: "#91A6FF",
+		name: "Cornflower Blue",
+	},
+	{
+		hex: "#FF88DC",
+		name: "Persian Pink",
+	},
+	{
+		hex: "#80FF72",
+		name: "Screamin Green",
+	},
+	{
+		hex: "#FF5154",
+		name: "Tart Orange",
+	},
+];
 
-export default function ColorRenderer () {
-  return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
-  )
+export default function ColorRenderer() {
+	return (
+		<div>
+			<h2>
+				Use the Color component to render each item in the colors array on the
+				page!
+			</h2>
+			{colors.map((color) => {
+				return <Color key={color.hex} hex={color.hex} name={color.name} />;
+			})}
+		</div>
+	);
 }
