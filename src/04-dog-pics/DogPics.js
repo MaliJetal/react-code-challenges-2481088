5e011e3c8366673fa5ade07ed2e7ec50c471c758
Jetal Mali <jetmali@publicisgroupe.net>
2022-10-24 02:07:04 +0530

    Challenge 4- My Solution

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,30 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
-  return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
-    </div>
-  )
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function DogPics() {
+	const [isloading, setIsloading] = useState("false");
+	const [imgSrc, setImgSrc] = useState("");
+
+	const getDogsData = async () => {
+		const dogs = await axios.get("https://dog.ceo/api/breeds/image/random");
+		return dogs.data.message;
+	};
+
+	useEffect(() => {
+		setIsloading("true");
+		getDogsData()
+			.then((src) => {
+				setImgSrc(src);
+			})
+			.catch((err) => console.log(err));
+
+		setIsloading("false");
+	}, []);
+
+	return (
+		<div className='dog-pics'>
+			<img src={imgSrc} alt={imgSrc} />
+			<button onClick={async (e) => setImgSrc(await getDogsData())}>🐶</button>
+		</div>
+	);
 }
