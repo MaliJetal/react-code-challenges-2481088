01605b5e132183f6d5c7f50af2cef9105a17c248
Jetal Mali <jetmali@publicisgroupe.net>
2022-11-20 13:36:21 +0530

    Challenged SOlved - 05

--- src/05-score-keeper/ScoreKeeper.js
+++ src/05-score-keeper/ScoreKeeper.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react'
 
 export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(Number(localStorage.getItem("score")) || 0);
+
+  useEffect(()=> {
+    localStorage.setItem("score",score);
+  }, [score]);
 
   return (
     <div>
       <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <button onClick={() => setScore(prevScore => 1 + Number(prevScore))}>+</button>
+      <button onClick={() => setScore(prevScore => prevScore -1 )}>-</button>
     </div>
   )
 }
